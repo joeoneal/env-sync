753f626932a2cbffc6638bd03eea739a7a85c085
Joe O'Neal <joeoneal641@gmail.com>
2026-02-10 01:06:17 -0700

    register method

--- server/app.py
+++ server/app.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
-from db_models import db 
+from flask_bcrypt import Bcrypt
+from db_models import db, User
 
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
 
 # postgresql://[user]:[password]@[host]:[port]/[database_name]
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/envsync_db'
@@ -15,7 +17,26 @@ migrate = Migrate(app, db)
 
 @app.route('/register', method=['POST'])
 def register():
-    pass
+    data = request.get_json()
+
+    if not data or not data.get('email') or not data.get('password'):
+        return jsonify({'error': 'Missing email or password'}), 400
+    
+    if User.query.filter_by(email=data['email']).first():
+        return jsonify({'error': 'User already exists with this email'}), 409
+    
+    hash = bcrypt.generate_password_hash(data['password'])
+    hashed_pw = hash.decode('utf-8')
+
+    user = User(email = data['email'], password = hashed_pw)
+
+    try: 
+        db.session.add(user)
+        db.sessions.commit()
+        return jsonify({'message' : 'User created successfully.'}), 201
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({'error' : 'Database error as follows', 'details' : str(e)}), 500
 
 @app.route('/login', method=['GET'])
 def login():
