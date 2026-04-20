7c3e85bd0479057c1f64d2badf235aaddbd1a2a6
Joe O'Neal <joeoneal641@gmail.com>
2026-04-19 18:09:04 -0600

    fixed url

--- cli/utils/config.py
+++ cli/utils/config.py
@@ -1,7 +1,7 @@
 import os
 
 TOKEN_FILE = os.path.expanduser("~/.envsync_config")
-BASE_URL = "env-sync.up.railway.app"
+BASE_URL = "https://env-sync.up.railway.app"
 
 PRIVATE_KEY_FILE = os.path.expanduser("~/.envsync_private.pem")
 PUBLIC_KEY_FILE = os.path.expanduser("~/.envsync_public.pem")
