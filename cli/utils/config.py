b148848f5511adb0f50645738dc99b5fba39a3bc
Joe O'Neal <joeoneal641@gmail.com>
2026-04-19 18:03:41 -0600

    updated base url to live hosted Railway url

--- cli/utils/config.py
+++ cli/utils/config.py
@@ -1,7 +1,7 @@
 import os
 
 TOKEN_FILE = os.path.expanduser("~/.envsync_config")
-BASE_URL = "http://127.0.0.1:7070"
+BASE_URL = "env-sync.up.railway.app"
 
 PRIVATE_KEY_FILE = os.path.expanduser("~/.envsync_private.pem")
 PUBLIC_KEY_FILE = os.path.expanduser("~/.envsync_public.pem")
